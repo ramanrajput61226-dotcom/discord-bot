2407078b7560a6055f4fd12650a836cd9656acb0
Raman Parmar <ramanrajput61226@gmail.com>
2026-08-08 17:08:16 +0530

    Update keep_alive.py

--- keep_alive.py
+++ keep_alive.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask
 from threading import Thread
 
@@ -5,10 +6,12 @@ app = Flask('')
 
 @app.route('/')
 def home():
-    return "RavageRaze Bot Online Hai!"
+    return "RavageRaze Bot Online!"
 
 def run():
-    app.run(host='0.0.0.0', port=8080)
+
+    port = int(os.environ.get("PORT", 8080))
+    app.run(host='0.0.0.0', port=port)
 
 def keep_alive():
     t = Thread(target=run)
